9556aebdfa90c2e2ab0e0aa24200348b451755c3
Fancy Pants <edaily94@gmail.com>
2023-03-21 13:58:53 -0700

    added outline for JS sheet

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+var userInput
+
+
+//Need Wheel JS
+
+//need local storage capability
+
+//randomize stored user choices
+
+//need local recommendation section
+
+//need map section
+
+//local accurate time
+
+//need place to store autofill for cities/locations (JQuery? API?)
+
+//need place to store autofill for food suggections (jQuery?)
+
+//need buttons
+
+//weather API
+
+//contact form event listener
