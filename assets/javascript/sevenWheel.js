3ca1545f067f41e1850905ffed12a62082912561
ctvb <deenicks@hotmail.com>
2023-03-21 22:28:22 -0500

    I'm just trying to be able to pull and the terminal is making me commit to get to the main branch to do that. I thought I commited earlier, but idk

---
+++ assets/javascript/sevenWheel.js
@@ -0,0 +1,5 @@
+// wheel page that controles wheel movement and the places that show up in them 
+
+// wheel page
+    // ANIMATE WHEEL SPIN SOMEHOW
+    // link to the website when clicked on choice 
