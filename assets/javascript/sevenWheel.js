8115d91074aa4a4af3e60b8f2310ce0b55fe12ff
Fancy Pants <edaily94@gmail.com>
2023-03-22 11:11:45 -0700

    changed labels on wheel slices and commented out wheel values 7-9

--- assets/javascript/sevenWheel.js
+++ assets/javascript/sevenWheel.js
@@ -15,15 +15,17 @@ var padding = {top:20, right:40, bottom:0, left:0},
         //(ED) each of these labels need to be tied to entries in userInput. Do we need to make a specific variable for each userInput? or should we store all of them in an array and break them up later?
 
         var data = [
-                    {"label":"Dell LAPTOP",  "value":1,  "question":"What CSS property is used for specifying the area between the content and its border?"}, // padding
-                    {"label":"IMAC PRO",  "value":2,  "question":"What CSS property is used for changing the font?"}, //font-family
-                    {"label":"SUZUKI",  "value":3,  "question":"What CSS property is used for changing the color of text?"}, //color
-                    {"label":"HONDA",  "value":4,  "question":"What CSS property is used for changing the boldness of text?"}, //font-weight
-                    {"label":"FERRARI",  "value":5,  "question":"What CSS property is used for changing the size of text?"}, //font-size
-                    {"label":"APARTMENT",  "value":6,  "question":"What CSS property is used for changing the background color of a box?"}, //background-color
-                    {"label":"IPAD PRO",  "value":7,  "question":"Which word is used for specifying an HTML tag that is inside another tag?"}, //nesting
-                    {"label":"LAND",  "value":8,  "question":"Which side of the box is the third number in: margin:1px 1px 1px 1px; ?"}, //bottom
-                    {"label":"MOTOROLLA",  "value":9,  "question":"What are the fonts that don't have serifs at the ends of letters called?"}, //sans-serif
+                    //(ED) changed individual bespoke wheel labels to "choice x" and text properties changed. also commented out values 7-9.
+                    
+                    {"label":"Choice 1",  "value":1,  "question":"What CSS property is used for specifying the area between the content and its border?"}, // padding
+                    {"label":"Choice 2",  "value":2,  "question":"What CSS property is used for changing the font?"}, //font-family
+                    {"label":"Choice 3",  "value":3,  "question":"What CSS property is used for changing the color of text?"}, //color
+                    {"label":"Choice 4",  "value":4,  "question":"What CSS property is used for changing the boldness of text?"}, //font-weight
+                    {"label":"Choice 5",  "value":5,  "question":"What CSS property is used for changing the size of text?"}, //font-size
+                    {"label":"Choice 6",  "value":6,  "question":"What CSS property is used for changing the background color of a box?"}, //background-color
+                    // {"label":"IPAD PRO",  "value":7,  "question":"Which word is used for specifying an HTML tag that is inside another tag?"}, //nesting
+                    // {"label":"LAND",  "value":8,  "question":"Which side of the box is the third number in: margin:1px 1px 1px 1px; ?"}, //bottom
+                    // {"label":"MOTOROLLA",  "value":9,  "question":"What are the fonts that don't have serifs at the ends of letters called?"}, //sans-serif
                     //{"label":"BMW", "value":10, "question":"With CSS selectors, what character prefix should one use to specify a class?"}
         ];
         var svg = d3.select('#chart')
