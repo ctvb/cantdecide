360a828db1d138207ac0a6e1977d7d2c75dbac6c
ctvb <deenicks@hotmail.com>
2023-03-23 21:16:24 -0500

    contact javascript started

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -1,3 +1,52 @@
-// contact page that links to githubs  
+// // contact page that links to githubs  
 
+// var fn
+// var ln
+// var email
+// var comment
+// var submit
+
+// // 
+// <form onsubmit="return false"></form>;
+
+// // INPUT BOX FOR FIRST NAME
+// <input type="text" placeholder="First Name" id="firstName" />;
+// <br></br>;
+
+// // INPUT BOX FOR lAST NAME
+// <input type="text" placeholder="Last Name" id="lastName" />;
+// <br></br>;
+
+// // INPUT BOX FOR EMAIL
+// <input type="email" placeholder="Email" id="email" />;
+// <br></br>;
+
+// // INPUT BOX FOR COMMENTS
+// <span>Comments</span>;
+// <br></br>;
+// <textarea id="comment" rows="4"></textarea>;
+// <br></br>;
+
+// // SUBMIT BUTTON
+// <button onclick="sendContact();">Send Contact</button>
+
+// // MAKE ALL INTO A FIELD
+// var fields = {};
+// document.addEventListener("DOMContentLoaded", function() {
+//     fields.firstName = document.getElementById('firstName');
+//     fields.lastName = document.getElementById('lastName');
+//     fields.email = document.getElementById('email');
+//     fields.comment = document.getElementById('comment');
+// });
+
+// // CHECKS IF THE FIELD IS EMPTY
+// function isNotEmpty(Value) {
+//     if (Value == null || typeof value == 'undefined' )return false;
+//     return (value.length >0);
+// }
+
+// // CHECKS IF THE FIELD VALUE IS A NUMBER
+// function isNumber(num) {
+//     return (num.length > 0) && !isNaN(num);
+// }
 
